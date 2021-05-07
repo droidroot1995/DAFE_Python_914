55b6908b1235bdb07f5a59c434866cbcbca0efb6
depinwhite <depinwhite@gmail.com>
2021-05-07 18:06:55 +0300

    added Conway's Game of Life

---
+++ game_of_life/libraries.py
@@ -0,0 +1,5 @@
+import sys
+import numpy as np
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QColorDialog, QInputDialog, QMenu
+from PyQt5.QtGui import QPainter, QColor, QPen, QBrush
+from PyQt5.QtCore import Qt, QTimer
---
+++ game_of_life/main.py
@@ -0,0 +1,6 @@
+from game import *
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Game()
+    sys.exit(app.exec_())
